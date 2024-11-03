3988dc19ad1ea3d7186c16db2f798427ed17db26
David-Solomon-J <44678769+David-Solomon-J@users.noreply.github.com>
2024-11-03 01:25:19 -0600

    Nite 1

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,13 +4,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  imgPath = '../media/cosmoGeekImg_1.jpg';
 }
